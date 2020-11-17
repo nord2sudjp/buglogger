4e41740ef061bdd04362015c05e28a6ed7f9380d
maka y <nord2sudjp@gmail.com>
2020-11-17 17:45:58 +0900

    Imlement AddLogItem

---
+++ src/components/AddLogItem.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
+const AddLogItem = () => {
+  const [text, setText] = useState("");
+  const [user, setUser] = useState("");
+  const [priority, setPriority] = useState("");
+
+  return (
+    <div>
+      <Card className="mt-5 mb-3">
+        <Card.Body>
+          <Form>
+            <Row className="my-3">
+              <Col>
+                <Form.Control
+                  placeholder="Log"
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                />
+              </Col>
+            </Row>
+            <Row className="my-3">
+              <Col>
+                <Form.Control
+                  placeholder="User"
+                  value={user}
+                  onChange={(e) => setUser(e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Form.Control
+                  as="select"
+                  placeholder="Priority"
+                  value={priority}
+                  onChange={(e) => setPriority(e.target.value)}
+                >
+                  <option value="0">Select Priority</option>
+                  <option value="low">Low</option>
+                  <option value="moderate">Moderate</option>
+                  <option value="high">High</option>
+                </Form.Control>
+              </Col>
+            </Row>
+            <Row className="my-3">
+              <Col>
+                <Button type="submit" variant="secondary" block>
+                  Save
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default AddLogItem;
